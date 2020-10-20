5c99a5847b0a0504c143d97dae795b951035ddf5
Max Dobeck <max.dobeck@saucelabs.com>
2020-10-20 14:28:36 -0700

    minor changes, may have introduced a problem...

--- hulu-89471.py
+++ hulu-89471.py
@@ -1,4 +1,3 @@
-import time
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
@@ -17,10 +16,34 @@ username = os.getenv("SAUCE_USER")
 access_key = os.getenv("SAUCE_KEY")
 sauce_client = SauceClient(username, access_key)
 
-desired_caps = {
+chrome_desired_caps = {
+    'platformName': "mac 10.15",
+    'browserName': "chrome",
+    'browserVersion': "latest",
+    # 'moz:firefoxOptions': {
+    #     'profile': "UEsDBBQAAAAIAGNiU1FuBzPD5wMAAKwNAAAHABwAdXNlci5qc1VUCQADquaNX6vmjV91eAsAAQT1AQAABBQAAACdV01v4zYQvfdXFD61QE0km+6le0qzKVBg0SzWCPZIUORIYkyRLD+s+N93KEprx5Yppydb8gw58+bNm3H04Kh1UP+yqpzp8Yl4VsPwXeqGdEz1zAEBzSoFYvXbzzVTHn799FM8eAoW0MYaF5JLC0yFNj8TNNpJvuCvIfTGbUkbgiW2lb7dr9PPlnm/VqCb0KLjh48f37pNATtgAj8EBOABxF/S+XCPsXAF6BZcPA3XdJjXK+WtMx1Qz520gbqoaZBdcrm7mb8osMoThEM/6QdlPMxnM1kL02tlmMC7NGsSsK3pv7egN4hWQmre3WJQNvh0Ge2MYGoBe9gFY5Qn4Jxx3Ghv1DHcM+lPHjn/62wFVLFJSeTw1niRRrSl0Vf6OehMgHXpMi8bbXSyhK4Ctxke/QWUwHXSe7w/3VGzqAJpwKDx7Xw1PGTrYJDNDnzEyteYC+WO+bZcSSX1lhgLmsJrAKeZSnR8dw/UiZjfop6/rIdKOLnDZ8Y5ICGjRog8EppycMHPQvaDa9JbxfYgHm9vNv8Y5H4i5+/zxvhKPOnvUiND/e0NkXiRef72BT1WrDIx/FEpprer867JCVGmFH3xCEaKU+Yancf2aiXCjbakb2UAJX3AA/6N0l3icqLMVgYy6sHUN5hcIk0RAMWwMNjC5WbxqfOiJS3y3uLZtAfFkwZEl0q6Okk5VVtnjkWLBQaiEdp6/4wm71bCaJMYPBbDAx6dDHtkigL8RnxwkgdqnGykptYoyfdlqmroUTjImE+hmm8CzSeT9HITq7Gxvg4v7wcyXqf8s8cUM/7RnWNhUlHQ8oL+HvoQ0+wH/p43oqlrtIM1s9YTZJ/pabWno0bM684EO/d25M98tOd8KLNtOvfpYfP1yPRmnvXYIqh8we1HjR1sPyzZFiOYgHt8/nJP7gg71PIchRe2Y3kUFifCacWmVirTbZrwnKFg7GCdGMPU+qrt4DxnBy/DpC8GmDshhVY+/qioLAbzOYvNhiPPkq7dLu8CT8jIxbMrZfh20MCrKuaBOd6ONFvaMzqSm4GI2Nli9bAprqLuG+IYHEpOinkBPtOsU7G6m2dCFvf11K1IpxAvzPopmNH2ghKNS93yNteBkIw0nV1PAyYj8ljA7Wg4+2F1OAxnVLk4jIMi2ola84Ef9Z3J03RYFP/WD3mXu7jCjmORDoIoEHrd0DRiFojVAk5k3gLffs6a+Gf+5f0TkzAhZIqYLQyb2X8W//cfhTJNk16Oq25xvBz135DxA+oqC7KSKjFWy6YNamGgngEwdQPpcJcc2LhKi6qDk6QP60/aa5HduHQu7D+H5le1jzavDYfN5D9QSwECHgMUAAAACABjYlNRbgczw+cDAACsDQAABwAYAAAAAAABAAAApIEAAAAAdXNlci5qc1VUBQADquaNX3V4CwABBPUBAAAEFAAAAFBLBQYAAAAAAQABAE0AAAAoBAAAAAA=",
+    #     # 'args': [
+    #     #     "-start-debug-server",
+    #     #     "9222"
+    #     # ]
+    # },
+    'sauce:options': {
+        'build':"sauce_debug10/20:chrome",
+        'name': "Slow Selenium CMDs",
+        'parentTunnel': "huluadmin",
+        'tunnelIdentifier': "ha-parent-robocop-saucelabs",
+        # 'seleniumVersion': "3.141.59",
+        'screenResolution': "2360x1770",
+        'username': username,
+        'accessKey': access_key,
+        # 'extendedDebugging': True,
+    },
+}
+
+firefox_desired_caps = {
     'platformName': "mac 10.15",
     'browserName': "firefox",
-    'browserVersion': "81",
+    'browserVersion': "latest",
     'moz:firefoxOptions': {
         'profile': "UEsDBBQAAAAIAGNiU1FuBzPD5wMAAKwNAAAHABwAdXNlci5qc1VUCQADquaNX6vmjV91eAsAAQT1AQAABBQAAACdV01v4zYQvfdXFD61QE0km+6le0qzKVBg0SzWCPZIUORIYkyRLD+s+N93KEprx5Yppydb8gw58+bNm3H04Kh1UP+yqpzp8Yl4VsPwXeqGdEz1zAEBzSoFYvXbzzVTHn799FM8eAoW0MYaF5JLC0yFNj8TNNpJvuCvIfTGbUkbgiW2lb7dr9PPlnm/VqCb0KLjh48f37pNATtgAj8EBOABxF/S+XCPsXAF6BZcPA3XdJjXK+WtMx1Qz520gbqoaZBdcrm7mb8osMoThEM/6QdlPMxnM1kL02tlmMC7NGsSsK3pv7egN4hWQmre3WJQNvh0Ge2MYGoBe9gFY5Qn4Jxx3Ghv1DHcM+lPHjn/62wFVLFJSeTw1niRRrSl0Vf6OehMgHXpMi8bbXSyhK4Ctxke/QWUwHXSe7w/3VGzqAJpwKDx7Xw1PGTrYJDNDnzEyteYC+WO+bZcSSX1lhgLmsJrAKeZSnR8dw/UiZjfop6/rIdKOLnDZ8Y5ICGjRog8EppycMHPQvaDa9JbxfYgHm9vNv8Y5H4i5+/zxvhKPOnvUiND/e0NkXiRef72BT1WrDIx/FEpprer867JCVGmFH3xCEaKU+Yancf2aiXCjbakb2UAJX3AA/6N0l3icqLMVgYy6sHUN5hcIk0RAMWwMNjC5WbxqfOiJS3y3uLZtAfFkwZEl0q6Okk5VVtnjkWLBQaiEdp6/4wm71bCaJMYPBbDAx6dDHtkigL8RnxwkgdqnGykptYoyfdlqmroUTjImE+hmm8CzSeT9HITq7Gxvg4v7wcyXqf8s8cUM/7RnWNhUlHQ8oL+HvoQ0+wH/p43oqlrtIM1s9YTZJ/pabWno0bM684EO/d25M98tOd8KLNtOvfpYfP1yPRmnvXYIqh8we1HjR1sPyzZFiOYgHt8/nJP7gg71PIchRe2Y3kUFifCacWmVirTbZrwnKFg7GCdGMPU+qrt4DxnBy/DpC8GmDshhVY+/qioLAbzOYvNhiPPkq7dLu8CT8jIxbMrZfh20MCrKuaBOd6ONFvaMzqSm4GI2Nli9bAprqLuG+IYHEpOinkBPtOsU7G6m2dCFvf11K1IpxAvzPopmNH2ghKNS93yNteBkIw0nV1PAyYj8ljA7Wg4+2F1OAxnVLk4jIMi2ola84Ef9Z3J03RYFP/WD3mXu7jCjmORDoIoEHrd0DRiFojVAk5k3gLffs6a+Gf+5f0TkzAhZIqYLQyb2X8W//cfhTJNk16Oq25xvBz135DxA+oqC7KSKjFWy6YNamGgngEwdQPpcJcc2LhKi6qDk6QP60/aa5HduHQu7D+H5le1jzavDYfN5D9QSwECHgMUAAAACABjYlNRbgczw+cDAACsDQAABwAYAAAAAAABAAAApIEAAAAAdXNlci5qc1VUBQADquaNX3V4CwABBPUBAAAEFAAAAFBLBQYAAAAAAQABAE0AAAAoBAAAAAA=",
         # 'args': [
@@ -29,7 +52,7 @@ desired_caps = {
         # ]
     },
     'sauce:options': {
-        'build':"sauce_debug10/19",
+        'build':"sauce_debug10/20",
         'name': "Slow Selenium CMDs",
         'parentTunnel': "huluadmin",
         'tunnelIdentifier': "ha-parent-robocop-saucelabs",
@@ -37,7 +60,6 @@ desired_caps = {
         'screenResolution': "2360x1770",
         'username': username,
         'accessKey': access_key,
-        'tags': ["custom_ff_profile"],
         # 'extendedDebugging': True,
     },
 }
@@ -48,7 +70,7 @@ def ready_state_check(driver):
     """
     driver.execute_script("return document.readyState")
 
-driver = webdriver.Remote(command_executor="https://ondemand.us-west-1.saucelabs.com/wd/hub", desired_capabilities=desired_caps)
+driver = webdriver.Remote(command_executor="https://ondemand.us-west-1.saucelabs.com/wd/hub", desired_capabilities=firefox_desired_caps)
 try:
     wait = WebDriverWait(driver, 12)
     find_by_css = lambda css_selector: wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
@@ -77,9 +99,9 @@ try:
     action_btn.click()
     ready_state_check(driver)
     ready_state_check(driver)
-    email_field = driver.find_element_by_css_selector("*[name='email']")
+    email_field = find_by_css("*[name='email']")
     email_field.send_keys(os.getenv("APP_USER"))
-    pw_field = driver.find_element_by_css_selector("*[name='password']")
+    pw_field = find_by_css("*[name='password']")
     pw_field.send_keys(os.getenv("APP_PW"))
     pw_field.send_keys(Keys.ENTER)
     ready_state_check(driver)
